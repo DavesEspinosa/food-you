c4eb215b26908610b4d323cc92829d02ce8fcf2b
David Espinosa <davesespinosa@gmail.com>
2020-07-31 18:59:45 +0200

    recipe added

--- bin/seeds.js
+++ bin/seeds.js
@@ -39,7 +39,23 @@ const recipes = [
     cuisine: "Vegan",
     video: "https://www.youtube.com/watch?v=0CH1rhKThk4",
     image: "https://www.deliciousmagazine.co.uk/wp-content/uploads/2020/02/159-Greek-zucchhini-frittata-768x960.jpg", 
-    duration: 25}
+    duration: 25},
+    {
+      author: null,
+      title: "Greek courgette and feta frittata",
+      inspiration: "In this recipe, quintessential Greek ingredients – olive oil, courgettes, tomatoes and feta – make this frittata sing with delicious light, summer flavours. Enjoy warm, cold for a picnic or in a packed lunch.",
+      ingredients: [
+        "olive oil",
+        "medium courgettes",
+        "garlic cloves",
+        "ripe tomatoes", 
+        "medium free-range eggs",
+        "Handful fresh dill",
+        "vegetarian feta"],
+      cuisine: "Vegan",
+      video: "https://www.youtube.com/watch?v=0CH1rhKThk4",
+      image: "https://www.deliciousmagazine.co.uk/wp-content/uploads/2020/02/159-Greek-zucchhini-frittata-768x960.jpg", 
+      duration: 25}
 ]
 
 const users = [
