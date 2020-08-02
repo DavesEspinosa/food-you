5eed3d92b4b8fd609d3b06bab492297cdbe580da
David Espinosa <davesespinosa@gmail.com>
2020-08-02 14:33:48 +0200

    layout header and footer, responsive and dynamic with users

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home unlogged page. */
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
 module.exports = router;
