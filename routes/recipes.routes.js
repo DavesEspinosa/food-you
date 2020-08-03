d585e0ca1076dc7cc09f195645bba81a018e560e
David Espinosa <davesespinosa@gmail.com>
2020-08-03 15:14:24 +0200

    recipe-details

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -6,23 +6,21 @@ const User = require("../models/user.model");
 const UserRecipe = require("../models/user-recipe.model");
 
 router.use((req, res, next) => {
-  if(req.session.currentUser){
+  if (req.session.currentUser) {
     next();
     return;
   }
   // si no hay ning'un usuario le redige al Home
-  res.redirect('/')
-})
+  res.redirect("/");
+});
 
 router.get("/list-recipes", async (req, res, next) => {
   try {
-    const recipes = await Recipe.find({author: null});
-    
-    recipes.length === 0 ?
-    res.render("recipes/list-recipes", { errorMessage: true})
-    :
-    res.render("recipes/list-recipes", { recipes });
+    const recipes = await Recipe.find({ author: null });
 
+    recipes.length === 0
+      ? res.render("recipes/list-recipes", { errorMessage: true })
+      : res.render("recipes/list-recipes", { recipes });
   } catch (error) {
     console.log(error);
     next(error);
@@ -33,8 +31,8 @@ router.get("/list-recipes", async (req, res, next) => {
 router.get("/own-recipes", async (req, res, next) => {
   try {
     const { _id } = req.session.currentUser;
-    
-    const recipes = await UserRecipe.find({author: _id});
+
+    const recipes = await UserRecipe.find({ author: _id });
     res.render("recipes/own-recipes", { recipes });
   } catch (error) {
     console.log(error);
@@ -56,8 +54,19 @@ router.get("/add-recipe", async (req, res, next) => {
 router.post("/add-recipe", async (req, res, next) => {
   try {
     const { _id } = req.session.currentUser;
-    const recipes = await UserRecipe.find({author: _id});
+    const recipes = await UserRecipe.find({ author: _id });
     res.render("recipes/add-recipe", { recipes });
+  } catch (error) {
+    console.log(error);
+    next(error);
+    return;
+  }
+});
+
+router.get("/recipe/:id", async (req, res, next) => {
+  try {
+    const recipe = await Recipe.findById({_id:req.params.id});
+    res.render("recipes/recipe-details", {recipe});
   } catch (error) {
     console.log(error);
     next(error);
