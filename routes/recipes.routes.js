fb3c33a2de8232db998e58c8ff025885450a6adc
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-08-03 12:10:05 +0200

    error message added

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Recipe = require("../models/recipe.model");
 const User = require("../models/user.model");
+const UserRecipe = require("../models/user-recipe.model");
 
 router.use((req, res, next) => {
   if(req.session.currentUser){
@@ -15,8 +16,48 @@ router.use((req, res, next) => {
 
 router.get("/list-recipes", async (req, res, next) => {
   try {
-    const recipes = await Recipe.find();
+    const recipes = await Recipe.find({author: null});
+    
+    recipes.length === 0 ?
+    res.render("recipes/list-recipes", { errorMessage: true})
+    :
     res.render("recipes/list-recipes", { recipes });
+
+  } catch (error) {
+    console.log(error);
+    next(error);
+    return;
+  }
+});
+
+router.get("/own-recipes", async (req, res, next) => {
+  try {
+    const { _id } = req.session.currentUser;
+    
+    const recipes = await UserRecipe.find({author: _id});
+    res.render("recipes/own-recipes", { recipes });
+  } catch (error) {
+    console.log(error);
+    next(error);
+    return;
+  }
+});
+
+router.get("/add-recipe", async (req, res, next) => {
+  try {
+    res.render("recipes/add-recipe");
+  } catch (error) {
+    console.log(error);
+    next(error);
+    return;
+  }
+});
+
+router.post("/add-recipe", async (req, res, next) => {
+  try {
+    const { _id } = req.session.currentUser;
+    const recipes = await UserRecipe.find({author: _id});
+    res.render("recipes/add-recipe", { recipes });
   } catch (error) {
     console.log(error);
     next(error);
