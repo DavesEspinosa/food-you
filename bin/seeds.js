94af5c0c5fe32d86f32e70f105437db314a2e4f7
David Espinosa <davesespinosa@gmail.com>
2020-08-03 12:03:06 +0200

    home done and layout

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,96 +1,275 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Recipe = require('../models/recipe.model');
-const User = require('../models/user.model');
+const Recipe = require("../models/recipe.model");
+const User = require("../models/user.model");
 
-const dbName = 'food-you';
-mongoose.connect(`mongodb://localhost/${dbName}`, { 
+const dbName = "food-you";
+mongoose.connect(`mongodb://localhost/${dbName}`, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const recipes = [
   {
     author: null,
     title: "Whole roast turbot with clams, fennel and potatoes",
-    inspiration: "Serving a whole turbot may seem extravagant, but it’s simple to prepare and makes a memorable centrepiece. Cooking a whole fish makes an impressive dinner party main.",
+    inspiration:
+      "Serving a whole turbot may seem extravagant, but it’s simple to prepare and makes a memorable centrepiece. Cooking a whole fish makes an impressive dinner party main.",
     ingredients: [
       "small fennel bulbs",
       "waxy potatoes (such as charlotte)",
       "onion",
-      "chilli flakes", 
+      "chilli flakes",
       "fennel seeds",
       "olive oil",
       "live clams",
       "dry white wine",
-      "turbot"],
+      "turbot",
+    ],
     cuisine: "Mediterranian",
     video: "https://www.youtube.com/watch?v=A9T2WK3dMeI",
-    image: "https://www.deliciousmagazine.co.uk/wp-content/uploads/2019/06/turbot-768x960.jpg", 
-    duration: 50
+    image: "",
+    duration: 50,
   },
   {
     author: null,
     title: "Greek courgette and feta frittata",
-    inspiration: "In this recipe, quintessential Greek ingredients – olive oil, courgettes, tomatoes and feta – make this frittata sing with delicious light, summer flavours. Enjoy warm, cold for a picnic or in a packed lunch.",
+    inspiration:
+      "In this recipe, quintessential Greek ingredients – olive oil, courgettes, tomatoes and feta – make this frittata sing with delicious light, summer flavours. Enjoy warm, cold for a picnic or in a packed lunch.",
     ingredients: [
       "olive oil",
       "medium courgettes",
       "garlic cloves",
-      "ripe tomatoes", 
+      "ripe tomatoes",
       "medium free-range eggs",
       "fresh dill",
-      "vegetarian feta"],
+      "vegetarian feta",
+    ],
     cuisine: "Mediterranian",
     video: "https://www.youtube.com/watch?v=0CH1rhKThk4",
-    image: "https://www.deliciousmagazine.co.uk/wp-content/uploads/2020/02/159-Greek-zucchhini-frittata-768x960.jpg", 
-    duration: 25
+    image: "",
+    duration: 25,
   },
   {
     author: null,
     title: "Creamy vegan sausage and spinach pasta",
-    inspiration: "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
     ingredients: [
       "olive oil",
       "red onions",
       "vegan sausages",
-      "baby leaf spinach", 
+      "baby leaf spinach",
       "dried pasta rigatoni",
       "soya cream",
-      "flatleaf parsley"],
+      "flatleaf parsley",
+    ],
+    cuisine: "Mediterranian",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Mediterranian",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
     cuisine: "Vegan",
     video: "",
-    image: "https://www.deliciousmagazine.co.uk/wp-content/uploads/2020/05/WEB-READY-CreamyVeggieSausageSpinachPastaVegan_Option2-768x960.jpg", 
-    duration: 20
-  }
-
-]
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Vegan",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Vegan",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Vegan",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Asian",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Asian",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Asian",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+  {
+    author: null,
+    title: "Creamy vegan sausage and spinach pasta",
+    inspiration:
+      "Comforting and creamy, this plant-based pasta dish marries vegan sausages with fennel seeds, red onion and garlic for a satisfying savoury flavour, it will impress anyone",
+    ingredients: [
+      "olive oil",
+      "red onions",
+      "vegan sausages",
+      "baby leaf spinach",
+      "dried pasta rigatoni",
+      "soya cream",
+      "flatleaf parsley",
+    ],
+    cuisine: "Asian",
+    video: "",
+    image: "",
+    duration: 20,
+  },
+];
 
 const users = [
-  
   {
     name: "a",
-    firstname:  "a",
-    username:  "a",
+    firstname: "a",
+    username: "a",
     password: "$2a$15$ICydVO.GUykwgekvwCTJZOG83/mx6y1fFZypcBtVAj7498683LTuW",
-    email:  "a@a", 
-    firstname:  "a", 
+    email: "a@a",
+    firstname: "a",
     postcode: "a",
     address: "a",
     city: "a",
     phone: "admin",
-    cartList: []
-  }
+    cartList: [],
+  },
 ];
 
 Recipe.create(recipes, (err) => {
-  if(err) { throw(err) }
+  if (err) {
+    throw err;
+  }
   console.log(`Created ${recipes.length} recipes`);
-  mongoose.connection.close()
+  mongoose.connection.close();
 });
 
 User.create(users, (err) => {
-  if(err) { throw(err) }
+  if (err) {
+    throw err;
+  }
   console.log(`Created ${users.length} users`);
-  mongoose.connection.close()
+  mongoose.connection.close();
 });
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,12 +3,9 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET home unlogged page. */
-router.get('/home', function(req, res, next) {
   res.render('home');
 });
+
+
 
 module.exports = router;
