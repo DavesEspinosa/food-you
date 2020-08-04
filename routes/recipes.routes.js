002b95dd82b25d4d3e961414f2e65bec915f1dca
David Espinosa <davesespinosa@gmail.com>
2020-08-04 12:19:48 +0200

    details and delete done

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Recipe = require("../models/recipe.model");
 
+
 router.use((req, res, next) => {
   if (req.session.currentUser) {
     next();
@@ -29,12 +30,11 @@ router.get("/list-recipes", async (req, res, next) => {
 router.get("/own-recipes", async (req, res, next) => {
   try {
     const { _id } = req.session.currentUser;
-    
-    const recipes = await Recipe.find({author: _id});
-    recipes.length === 0 ?
-    res.render("recipes/own-recipes", { errorMessage: true })
-    :
-    res.render("recipes/own-recipes", { recipes });
+
+    const recipes = await Recipe.find({ author: _id });
+    recipes.length === 0
+      ? res.render("recipes/own-recipes", { errorMessage: true })
+      : res.render("recipes/own-recipes", { recipes });
   } catch (error) {
     console.log(error);
     next(error);
@@ -49,7 +49,26 @@ router.get("/add-recipe", (req, res, next) => {
 router.post("/add-recipe", async (req, res, next) => {
   try {
     const { _id } = req.session.currentUser;
-    await Recipe.create({ ...req.body, author: _id });
+    //req.body para sacar video
+    //crear un arrow function de extractkey
+    const { video } = req.body;
+    const key =  (video) => {
+      let arr = [...video];
+      let result = "";
+      if (arr.length > 43) {
+        // si es una lista
+        let index = arr.findIndex((letter) => letter === "=");
+        let endIndex = arr.findIndex((letter) => letter === "&");
+        let extract = arr.slice(index + 1, endIndex);
+        extract.forEach((letter) => (result += letter));
+        return result;
+      }
+      let index = arr.findIndex((letter) => letter === "=");
+      let extract = arr.slice(index + 1, arr.length);
+      extract.forEach((letter) => (result += letter));
+      return result;
+    };
+    await Recipe.create({ ...req.body, video: key(video), author: _id });
     res.redirect("/own-recipes");
   } catch (error) {
     console.log(error);
@@ -60,9 +79,20 @@ router.post("/add-recipe", async (req, res, next) => {
 
 router.get("/recipe/:id", async (req, res, next) => {
   try {
-    const recipe = await Recipe.findById({_id:req.params.id});
+    const recipe = await Recipe.findById({ _id: req.params.id });
     console.log(recipe);
-    res.render("recipes/recipe-details", {recipe});
+    res.render("recipes/recipe-details", { recipe });
+  } catch (error) {
+    console.log(error);
+    next(error);
+    return;
+  }
+});
+
+router.post("/:id/delete", async (req, res, next) => {
+  try {
+    await Recipe.findByIdAndRemove({ _id: req.params.id });
+    res.redirect("/own-recipes");
   } catch (error) {
     console.log(error);
     next(error);
