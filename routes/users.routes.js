60e20ea428ee8814d9bb0abe07e6e2008fd8c78e
David Espinosa <davesespinosa@gmail.com>
2020-08-04 18:30:07 +0200

    Edit done, post left

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/user.model');
+const User = require("../models/user.model");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/profile', function(req, res, next) {
-  res.render('profile/user-profile');
+router.get("/profile", function (req, res, next) {
+  res.render("profile/user-profile");
 });
 
-router.get('/profile/edit', function(req, res, next) {
-  res.render('profile/edit-profile');
+router.get("/edit-profile", function (req, res, next) {
+  res.render("profile/edit-profile");
 });
 
 module.exports = router;
