67441fca984d394b6166d001d88f52d6b4c6d6b4
David Espinosa <davesespinosa@gmail.com>
2020-08-07 11:00:14 +0200

    done

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+//app.use(express.static(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
